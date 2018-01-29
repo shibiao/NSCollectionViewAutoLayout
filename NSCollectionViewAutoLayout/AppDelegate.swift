b9b26533ad6a1190341b2e0095537687aee666c8
WalkWithWind <956035125@qq.com>
2018-01-29 15:04:31 +0800

    Initial Commit

---
+++ NSCollectionViewAutoLayout/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NSCollectionViewAutoLayout
+//
+//  Created by sycf_ios on 2018/1/29.
+//  Copyright © 2018年 sycf_ios. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
