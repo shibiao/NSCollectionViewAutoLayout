726778d2fc4a95ae5e6477e1060862d670291e8e
WalkWithWind <956035125@qq.com>
2018-01-29 15:18:58 +0800

    init

---
+++ NSCollectionViewAutoLayout/CustomCellView.swift
@@ -0,0 +1,22 @@
+//
+//  CustomCellView.swift
+//  NSCollectionViewAutoLayout
+//
+//  Created by sycf_ios on 2018/1/29.
+//  Copyright © 2018年 sycf_ios. All rights reserved.
+//
+
+import Cocoa
+
+class CustomCellView: NSTableCellView {
+
+    @IBOutlet weak var label: NSTextField!
+    
+    
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
